96743a35ac030ee891a03e9dc05b677f6765b4ad
bryant tee <bryantt23@gmail.com>
2015-01-15 03:32:58 -0800

    3.6 d(one)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
+#   i guess this is reached with localhost:3000
   root 'static_pages#home'
+  
+  
   get  'static_pages/help'
   get  'static_pages/about'
+  get  'static_pages/contact'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,14 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+#   it's using this method to dry the title "Ruby on Rails Tutorial Sample App"
+#   @ is an instance variable
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
@@ -8,20 +16,27 @@ class StaticPagesControllerTest < ActionController::TestCase
     # assert_select method, which lets us test for the presence of a 
     # particular HTML tag (sometimes called a “selector” 
   
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    # assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
     
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact " do
+    get :contact 
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
