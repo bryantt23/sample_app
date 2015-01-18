8bca1ce685b23d6b33567a1b20d2d0e31c6725e1
bryant tee <bryantt23@gmail.com>
2015-01-18 00:52:41 -0800

    5.3.2d

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
   
+  
+  # root method arranges for the root path / to be routed to a 
+  # controller and action of our choice
+
+  # also allows us to refer to routes by a name rather than by the raw URL
+  
 #   i guess this is reached with localhost:3000
   root 'static_pages#home'
   
   
-  get  'static_pages/help'
-  get  'static_pages/about'
-  get  'static_pages/contact'
+  # routes a GET request for the URL /help to the help action in the 
+  # Static Pages controller, so that we can use the URL /help in place of the 
+  # more verbose /static_pages/help  
+  
+  # get  'static_pages/help'  
+  get 'help'    => 'static_pages#help'
+  
+  
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+
 end
