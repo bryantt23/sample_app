01c12c09cd8ddeda164bb54fdd8cf8c3bb067dc1
bryant tee <bryantt23@gmail.com>
2015-01-21 02:28:47 -0800

    6.2.1d

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+=begin
+
+To write a test for a valid object, we'll create 
+an initially valid User model object @user using 
+the special setup method, which automatically 
+gets run before each test. 
+Because @user is an instance variable, 
+it's automatically available in all the tests, 
+and we can test its validity using the 
+valid? method 
+
+=end
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
 end
