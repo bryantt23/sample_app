10be3d890b0db95e57cfc3d0485bd7f12a4ef445
bryant tee <bryantt23@gmail.com>
2015-01-21 02:43:42 -0800

    6.2.2d

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -22,4 +22,17 @@ valid? method
   test "should be valid" do
     assert @user.valid?
   end
+  
+    
+# test for validation of the name attribute
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+  
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
 end
