eb255e1442cf76e9b043a2a2daa9840a09119dd0
bryant tee <bryantt23@gmail.com>
2015-01-21 07:15:54 -0800

    6.4d

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,34 @@
 class User < ActiveRecord::Base
+  
+  
   before_save { self.email = email.downcase }
+  # before_save { email.downcase! }
+  
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   validates :email, presence: true, length: { maximum: 255 },
+  # validates :email, presence:   true,
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+  
+
+#   http://stackoverflow.com/questions/12142374/railstutorial-org-chapter-6-unknown-attribute-password
+ # attr_accessible :email, :name, :password, :password_confirmation
+ # has_secure_password
+  
+  
+#   copied from hartl's github
+  # before_save { |user| user.email = user.email.downcase }
+# 
+  # validates :name,  presence: true, length: { maximum: 50 }
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    # uniqueness: { case_sensitive: false }
+  # validates :password, length: { minimum: 6 }
+  # validates :password_confirmation, presence: true
+
+  
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -15,7 +15,8 @@ valid? method
 
 =end
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -71,6 +72,10 @@ valid? method
   end
   
   
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
   
   
   
