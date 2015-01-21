c8f8d19eb692c8f3cb24a19544cfcdbcaf955c37
bryant tee <bryantt23@gmail.com>
2015-01-21 04:53:26 -0800

    d

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,17 +4,16 @@ class UserTest < ActiveSupport::TestCase
 
 =begin
 
-To write a test for a valid object, we'll create 
-an initially valid User model object @user using 
-the special setup method, which automatically 
-gets run before each test. 
-Because @user is an instance variable, 
-it's automatically available in all the tests, 
-and we can test its validity using the 
-valid? method 
+To write a test for a valid object, we'll create
+an initially valid User model object @user using
+the special setup method, which automatically
+gets run before each test.
+Because @user is an instance variable,
+it's automatically available in all the tests,
+and we can test its validity using the
+valid? method
 
 =end
-
   def setup
     @user = User.new(name: "Example User", email: "user@example.com")
   end
@@ -22,20 +21,18 @@ valid? method
   test "should be valid" do
     assert @user.valid?
   end
-  
-    
-# test for validation of the name attribute
+
+  # test for validation of the name attribute
   test "name should be present" do
     @user.name = "     "
     assert_not @user.valid?
   end
-  
 
   test "email should be present" do
     @user.email = "     "
     assert_not @user.valid?
   end
-  
+
   test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
@@ -48,19 +45,35 @@ valid? method
 
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                         first.last@foo.jp alice+bob@baz.cn]
+    first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
-    
-  test "email validation should reject invalid addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
-    invalid_addresses.each do |invalid_address|
-      @user.email = invalid_address
-      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
+
+    test "email validation should reject invalid addresses" do
+      invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+      foo@bar_baz.com foo@bar+baz.com]
+      invalid_addresses.each do |invalid_address|
+        @user.email = invalid_address
+        assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+      end
+    end
+
+
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
   end
-end
+  
+  
+  
+  
+  
+  
+
 end
