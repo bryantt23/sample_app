c80b167fc3aff6fae9a25d71b18befafad4eaebd
bryant tee <bryantt23@gmail.com>
2015-01-26 01:32:58 -0800

    7.3.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,29 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+    
+  def create
+    @user = User.new(user_params)
+    # @user = User.new(params[:user])    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
   
   
   
   
+  # Since user_params will only be used internally by the 
+  # Users controller and need not be exposed to 
+  # external users via the web, we’ll make it private 
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
   
 end
