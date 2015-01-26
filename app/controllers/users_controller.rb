d084ff4824ff510d27e761a4d422cf7b89ac8cb3
bryant tee <bryantt23@gmail.com>
2015-01-26 01:01:04 -0800

    7.1d

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,9 @@ class UsersController < ApplicationController
 #     instance variable = find method on User model to retrieve from 
 #     database, uses params to retrieve user id 
     @user = User.find(params[:id])
+
+# 7.1.3 uses the byebug gem 
+    # debugger
   end
   
   def new
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user)
+    
+    # Since email addresses are case-insensitive (Section 6.2.4) but 
+      # MD5 hashes are not, we’ve used the downcase method to ensure 
+      # that the argument to hexdigest is all lower-case.
+     
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
