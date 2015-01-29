410ef7bb9b20212ef106b78e4fd6d1b154472314
bryant tee <bryantt23@gmail.com>
2015-01-29 01:46:15 -0800

    Revert "9.4d"
    This reverts commit ba4c52b95b0f4fbe4548074479abc684ee51091f.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -45,24 +45,7 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
   
-  # users who aren’t logged in should be redirected to the 
-  # login page  
-  test "should redirect destroy when not logged in" do
-    assert_no_difference 'User.count' do
-      delete :destroy, id: @user
-    end
-    assert_redirected_to login_url
-  end
-
-# users who are logged in but who aren’t admins should be 
-# redirected to the Home page
-  test "should redirect destroy when logged in as a non-admin" do
-    log_in_as(@other_user)
-    assert_no_difference 'User.count' do
-      delete :destroy, id: @user
-    end
-    assert_redirected_to root_url
-  end
+  
   
   
   
