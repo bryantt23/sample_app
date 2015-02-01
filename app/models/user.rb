7704f5ab25babb87572eb927dfd66a7bd9e3d8f3
bryant tee <bryantt23@gmail.com>
2015-02-01 02:19:51 -0800

    10d

--- app/models/user.rb
+++ app/models/user.rb
@@ -125,7 +125,7 @@ attribute
     update_attribute(:reset_digest,  User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
-
+ 
   # Sends password reset email.
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
