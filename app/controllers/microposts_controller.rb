6730414a7777e2d0748b205d88e1d42bb089bf03
bryant tee <bryantt23@gmail.com>
2015-02-02 03:37:56 -0800

    11.3.2d

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,9 +1,25 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
+# put the form on the Home page itself (i.e., the root path /
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
   end
+
+  private
+  
+# strong parameters via micropost_params, which permits only the 
+# micropost’s content attribute to be modified through the web.   
+def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
 
   def home
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
@@ -8,8 +9,7 @@ class StaticPagesController < ApplicationController
 
   def about
   end
-  
+
   def contact
   end
-
 end
